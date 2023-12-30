a9dfee327fe45dd74024c215a421631d4759c25a
Vitor <masteriw@gmail.com>
2023-12-30 03:11:40 -0300

    Modelo de dados atualizado para incluir classe TodoList, conjunto de itens Todo, chave estrangeira etc (falta mexer no CRUD para refletir mudanças no banco)

--- todoapp/app.py
+++ todoapp/app.py
@@ -12,9 +12,16 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   description = db.Column(db.String(), nullable = False)
   completed = db.Column(db.Boolean(), nullable = False, default = False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
   def __repr__(self):
       return f'<Todo> id = {self.id} description = {self.description} completed = {self.completed}>'
+  
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(), nullable = False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
 
 @app.route('/')
 def index():
